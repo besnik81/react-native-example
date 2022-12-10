6c354e017ee935ed2aa0cb2b5789806083d7f1f4
Besnik Selimi <Besnik.Selimi@inspire11.com>
2022-12-10 10:08:48 +0100

    Example modifications and reloading

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to stop working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.js to st on your app!</Text>
     </View>
   );
 }
